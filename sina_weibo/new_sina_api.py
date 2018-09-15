9c583ec59d32c07c6c0c4afe69c4f4c22aa380fc
Ying's VB <dragon5y@foxmail.com>
2018-09-15 00:34:38 -0700

    changed tokens for users API

--- sina_weibo/new_sina_api.py
+++ sina_weibo/new_sina_api.py
@@ -6,27 +6,31 @@ APP_SECRET = '0b08d0aa9557b57f9c8781299f82e774'
 CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'  
 client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
 
-url=client.get_authorize_url() 
-webbrowser.open_new(url)  
+#url=client.get_authorize_url() 
+#webbrowser.open_new(url)  
 
-print 'input text after code: '
+#print 'input text after code: '
 #code = raw_input()
 
 #r = client.request_access_token(code)
-access_token = '2.00NYjZ9C4mJpbDbd81d65c3dMqpTvC'
-expires_in = 1536865199
+#access_token = r.access_token
+#expires_in = r.expires_in
+access_token = '2.00xmlwlG4mJpbDe3ad46fd15odNhsD'
+expires_in = 1694674233
 
-##print(access_token)
-##print(expires_in)
+print(access_token)
+print(expires_in)
 
 client.set_access_token(access_token, expires_in)
-statuses = client.users__show(screen_name='alpacatuotuo')
+statuses = client.users__show(
+	access_token='2.00xmlwlG4mJpbDe3ad46fd15odNhsD',uid=2254519653)
 
 length = len(statuses)
 print length
 
-for i in range(0,length):
-    print 'Username: '+statuses[i]['screen_name']
-    print 'Info: '+statuses[i]['description']
-    print 'Location: '+statuses[i]['location']
-    print 'Weibo: '+statuses[i]['status']['text']
+
+print 'id: '+statuses['idstr']
+print 'Username: '+statuses['screen_name']
+print 'Info: '+statuses['description']
+print 'Location: '+statuses['location']
+print 'Weibo: '+statuses['status']['text']
