955dc0d9b547eb0b578eb2e527b8db1e7b9a052d
Ying's VB <dragon5y@foxmail.com>
2018-10-02 01:05:55 -0700

    added mongo integration

--- sina_weibo/new_sina_api.py
+++ sina_weibo/new_sina_api.py
@@ -1,11 +1,14 @@
 from weibo import APIClient
+from pymongo import MongoClient
+import requests
 import webbrowser
 
-APP_KEY = '3307315251'
-APP_SECRET = '0b08d0aa9557b57f9c8781299f82e774'
-CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'  
-client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
-
+# APP_KEY = '3307315251'
+# APP_SECRET = '0b08d0aa9557b57f9c8781299f82e774'
+# CALLBACK_URL = 'https://api.weibo.com/oauth2/default.html'  
+# client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK_URL)
+ACCESS_TOKEN = '2.00xmlwlG4mJpbDe3ad46fd15odNhsD'
+URL = 'https://api.weibo.com/2/comments/by_me.json'
 #url=client.get_authorize_url() 
 #webbrowser.open_new(url)  
 
@@ -15,22 +18,65 @@ client = APIClient(app_key=APP_KEY, app_secret=APP_SECRET, redirect_uri=CALLBACK
 #r = client.request_access_token(code)
 #access_token = r.access_token
 #expires_in = r.expires_in
-access_token = '2.00xmlwlG4mJpbDe3ad46fd15odNhsD'
-expires_in = 1694674233
+def getPage(pageurl):
+	params = {
+            'access_token': ACCESS_TOKEN
+        }
 
-print(access_token)
-print(expires_in)
+        try:
+        	response = requests.get(pageurl, params=params)
+        	if response.status_code == 200:
+					return response.json()
+    	except requests.ConnectionError as e:
+        	print('Error', e.args)
 
-client.set_access_token(access_token, expires_in)
-statuses = client.users__show(
-	access_token='2.00xmlwlG4mJpbDe3ad46fd15odNhsD',uid=2254519653)
-
-length = len(statuses)
-print length
+def run():
 
 
-print 'id: '+statuses['idstr']
-print 'Username: '+statuses['screen_name']
-print 'Info: '+statuses['description']
-print 'Location: '+statuses['location']
-print 'Weibo: '+statuses['status']['text']
+    while True:
+        # request statuses__public_timeline
+        comments = getPage(URL)
+
+        length = len(comments['comments'])
+        
+        for key, value in comments.items(): 
+        	print key, value
+
+        comments = comments['comments']
+        Monclient = MongoClient('localhost', 27017)
+        db = Monclient['Weibo']
+        WeiboData = db['HadSelected']
+
+        # for i in range(0, length):
+        i = 0
+        created_at = comments[i]['created_at']
+        id = comments[i]['user']['id']
+        province = comments[i]['user']['province']
+        city = comments[i]['user']['city']
+        followers_count = comments[i]['user']['followers_count']
+        friends_count = comments[i]['user']['friends_count']
+        statuses_count = comments[i]['user']['statuses_count']
+        url = comments[i]['user']['url']
+        nickname = comments[i]['user']['screen_name']
+        desc = comments[i]['user']['description']
+        location = comments[i]['user']['location']
+        text = comments[i]['text']
+
+        
+        WeiboData.insert_one({
+            'created_at': created_at,
+            'id': id,
+            'nickname': nickname,
+            'text': text,
+            'province': province,
+            'location': location,
+            'description': desc,
+            'city': city,
+            'followers_count': followers_count,
+            'friends_count': friends_count,
+            'statuses_count': statuses_count,
+            'url': url
+            })
+
+if __name__ == "__main__":
+    run()
